9c7a999afdbf53e5a9e9319e0c86f218c238e282
SriramKalki <sriramkalki@hotmail.com>
2024-06-21 13:55:05 -0500

    Edit/Delete Task

--- app/routes.py
+++ app/routes.py
@@ -71,3 +71,35 @@ def task(task_id):
     if task.user_id != current_user.id:
         abort(403)
     return render_template('task.html', title=task.title, task=task)
+
+@app.route('/task/<int:task_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if task.user_id != current_user.id:
+        abort(403)
+    form = TaskForm()
+    if form.validate_on_submit():
+        task.title = form.title.data
+        task.description = form.description.data
+        task.due_date = form.due_date.data
+        db.session.commit()
+        flash('Your task has been updated!')
+        return redirect(url_for('task', task_id=task.id))
+    elif request.method == 'GET':
+        form.title.data = task.title
+        form.description.data = task.description
+        form.due_date.data = task.due_date
+    return render_template('edit_task.html', title='Edit Task', form=form)
+
+@app.route('/task/<int:task_id>/delete', methods=['POST'])
+@login_required
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if task.user_id != current_user.id:
+        abort(403)
+    db.session.delete(task)
+    db.session.commit()
+    flash('Your task has been deleted!')
+    return redirect(url_for('index'))
+
